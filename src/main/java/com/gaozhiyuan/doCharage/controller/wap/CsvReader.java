3943d57d30a17b69e8fc7576ed70db8726942bfc
gzy <15504769182@163.com>
2025-02-26 11:58:35 +0800

    refactor(core): 使用构造函数注入并管理Bean生命周期

--- src/main/java/com/gaozhiyuan/doCharage/controller/wap/CsvReader.java
+++ src/main/java/com/gaozhiyuan/doCharage/controller/wap/CsvReader.java
@@ -3,23 +3,29 @@ package com.gaozhiyuan.doCharage.controller.wap;
 import com.gaozhiyuan.doCharage.model.LcElongHotels;
 import com.gaozhiyuan.doCharage.service.LcElongHotelsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class CsvReader {
 
+    private final LcElongHotelsService lcElongHotelsService;
+
     @Autowired
-    LcElongHotelsService lcElongHotelsService;
-
+    public CsvReader(LcElongHotelsService lcElongHotelsService) {
+        this.lcElongHotelsService = lcElongHotelsService;
+    }
     public void processCsvFiles() {
-        LcElongHotels lcElongHotels =  lcElongHotelsService.getById("53");
+        LcElongHotels lcElongHotels = lcElongHotelsService.getById("53");
         System.out.println(lcElongHotels);
-
     }
 
     public static void main(String[] args) {
-        CsvReader csvReader = new CsvReader();
+        // 在Spring Boot应用中，通常不需要手动创建Bean，Spring会自动管理Bean的生命周期
+        ApplicationContext context = SpringApplication.run(CsvReader.class, args);
+        CsvReader csvReader = context.getBean(CsvReader.class);
         csvReader.processCsvFiles();
     }
 }
